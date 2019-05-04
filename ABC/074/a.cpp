d7c73eba55aa0460f6c32b7a58746ac7f8d5d23f
Yusuke Uehara <uskey1024@gmail.com>
2019-05-05 01:23:08 +0900

    solve abc 084, 076, 074 a

---
+++ ABC/074/a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int N, A; cin >> N >> A;
+    cout << N * N - A << endl;
+}
---
+++ ABC/076/a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int R, G; cin >> R >> G;
+    cout << 2 * G - R << endl;
+}
